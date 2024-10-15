85799a284398dd8666724455e97c444d1b892c19
Fredrik Monsen <fredrik.monsen@nb.no>
2024-10-15 13:19:10 +0200

    feat: add auth provider and auth routes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import {AuthProvider} from '@/app/AuthProvider';
 import {NextUIProvider} from '@nextui-org/react';
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <AuthProvider>
+      <NextUIProvider>
+        {children}
+      </NextUIProvider>
+    </AuthProvider>
   );
 }
