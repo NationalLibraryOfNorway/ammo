1659eeab62a02235c7a7c21329cc07da8e7036fe
Fredrik Monsen <31658585+fredrikmonsen@users.noreply.github.com>
2024-10-17 09:58:11 +0200

    feat: setup authentication (TT-1755) (#2)
    * feat: add auth provider and auth routes
    
    * add simple header to display logged in user and logout button
    
    * use env variables from k8s secrets in deployment
    
    * add .env.example
    
    * exclude AuthProvider from StrictMode in local development to prevent auth problems
    
    * fix main page layout
    
    * run lint --fix
    
    * Apply suggestions

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import {AuthProvider} from '@/app/AuthProvider';
 import {NextUIProvider} from '@nextui-org/react';
+import React from 'react';
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <AuthProvider>
+      <React.StrictMode>
+        <NextUIProvider>
+          {children}
+        </NextUIProvider>
+      </React.StrictMode>
+    </AuthProvider>
   );
 }
